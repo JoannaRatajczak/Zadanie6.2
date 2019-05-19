fb79017139de37d9f46418ebb28d851d9e7fac37
JoannaRatajczak <joanna.jr.ratajczak@gmail.com>
2019-05-19 12:45:10 +0200

    Wprowadzanie imion

---
+++ src/TablicaImion.java
@@ -0,0 +1,14 @@
+public class TablicaImion {
+    public static void main(String[] args) {
+
+        String[] imiona = new String[3];
+        imiona[0]="Ciri";
+        imiona[1]="Geralt";
+        imiona[2]="Triss";
+
+        System.out.println(imiona[0]);
+        System.out.println(imiona[1]);
+        System.out.println(imiona[2]);
+
+    }
+}
